c1835fdf9ebc3524e70f3a4a655f1da34b51b8ca
milton839 <miltonbiswas839@gmail.com>
2026-07-13 17:30:52 +0600

    String concate solved raw way

--- string-concat.c
+++ string-concat.c
@@ -23,5 +23,11 @@ int main()
     char str2[] = " Biswas";
 
     str_concate(str1, str2);
+    printf("After concatenation: %s\n", str1);
+
+    char a[30] = "Hello";
+    char b[] = " World";
+    str_concate(a, b);
+    printf("After concatenation: %s\n", a);
     return 0;
 }
