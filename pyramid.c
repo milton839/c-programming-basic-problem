a61839b4e9c4ab04fcb60066610a4285da39fcf7
milton839 <miltonbiswas839@gmail.com>
2026-07-13 10:05:44 +0600

    right half pyramid

---
+++ pyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void right_half_pyramid_star(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int number = 6;
+    right_half_pyramid_star(number);
+    return 0;
+}
