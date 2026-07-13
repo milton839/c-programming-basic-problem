516388b0bfe64c2affe525bc2d7e21667ddebdad
milton839 <miltonbiswas839@gmail.com>
2026-07-13 11:09:10 +0600

    Diamond

--- pyramid.c
+++ pyramid.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void right_half_pyramid_star(int n){
+void right_half_pyramid_star(int n)
+{
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n-i-1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             printf(" ");
         }
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             printf("*");
         }
@@ -15,57 +16,76 @@ void right_half_pyramid_star(int n){
     }
 }
 
-void reverse_right_half_pyramid_star(int n){
+void reverse_right_half_pyramid_star(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i; j++)
         {
             printf(" ");
         }
-        for (int j = 0; j < n-i; j++)
+        for (int j = 0; j < n - i; j++)
         {
             printf("*");
         }
         printf("\n");
     }
-    
 }
 
-void full_pyramid_star(int n){
+void full_pyramid_star(int n)
+{
     for (int i = 0; i < n; i++)
     {
-        //Space printing
-        for (int j = 0; j < n-i-1; j++)
+        // Space printing
+        for (int j = 0; j < n - i - 1; j++)
         {
             printf(" ");
         }
-        //Star printing
-        for (int j = 0; j < 2*i+1; j++)
+        // Star printing
+        for (int j = 0; j < 2 * i + 1; j++)
         {
             printf("*");
         }
         printf("\n");
     }
-    
 }
 
-void reverse_full_pyramid_star(int n){
+void reverse_full_pyramid_star(int n)
+{
     for (int i = 0; i < n; i++)
     {
-        //Space printing
+        // Space printing
         for (int j = 0; j < i; j++)
         {
             printf(" ");
         }
-        //Star printing
-        for (int j = 0; j < 2*(n-i)-1; j++)
+        // Star printing
+        for (int j = 0; j < 2 * (n - i) - 1; j++)
         {
             printf("*");
         }
-       
+
         printf("\n");
     }
-    
+}
+
+void pyramid_diamond_star(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        // Space printing
+        int space_count = i < n ? n - i - 1 : i - n + 1;
+        for (int j = 0; j < space_count; j++)
+        {
+            printf(" ");
+        }
+        int star_count = i < n ? 2 * i + 1 : 2 * (2 * n - i - 2) + 1;
+        for (int j = 0; j < star_count; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
 int main()
 {
@@ -73,6 +93,7 @@ int main()
     // right_half_pyramid_star(number);
     // reverse_right_half_pyramid_star(number);
     // full_pyramid_star(number);
-    reverse_full_pyramid_star(number);
+    // reverse_full_pyramid_star(number);
+    pyramid_diamond_star(number);
     return 0;
 }
