d7b260b1c929248130ff01cd9e48b37291c4fcba
milton839 <miltonbiswas839@gmail.com>
2026-07-13 17:28:17 +0600

    String concate solved raw way

---
+++ string-concat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void str_concate(char str1[], char str2[]){
+    int i = 0, j = 0;
+
+    while(str1[i] != '\0'){
+        i++;
+    }
+    
+    while(str2[j] != '\0'){
+        
+        str1[i] = str2[j];
+        i++;
+        j++;
+        
+    }
+    
+    str1[i] = '\0';
+}
+int main()
+{
+    char str1[30] = "Milton";
+    char str2[] = " Biswas";
+
+    str_concate(str1, str2);
+    return 0;
+}
