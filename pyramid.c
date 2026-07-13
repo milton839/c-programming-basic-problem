dcc0571ed87cae4b53058c1be68fb1c36e8bae64
milton839 <miltonbiswas839@gmail.com>
2026-07-13 10:11:05 +0600

    reverse right half pyramid

--- pyramid.c
+++ pyramid.c
@@ -14,9 +14,26 @@ void right_half_pyramid_star(int n){
         printf("\n");
     }
 }
+
+void reverse_right_half_pyramid_star(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < n-i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
 int main()
 {
     int number = 6;
-    right_half_pyramid_star(number);
+    // right_half_pyramid_star(number);
+    reverse_right_half_pyramid_star(number);
     return 0;
 }
