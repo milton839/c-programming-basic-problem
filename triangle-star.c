7f7a425d80522989e97c50663f4ff80318c795aa
milton839 <miltonbiswas839@gmail.com>
2026-07-13 09:49:22 +0600

    reverse triangle print

--- triangle-star.c
+++ triangle-star.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
-int main()
-{
-    int number = 6;
 
-    for (int  i = 0; i < number; i++)
+int triangle_star(int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             printf("* ");
         }
         printf("\n");
     }
-    
+    return 0;
+}
+
+int reverse_triangle_star(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main()
+{
+    int number = 6;
+
+    // triangle_star(number);
+    reverse_triangle_star(number);
     return 0;
 }
