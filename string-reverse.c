55c4431b07f580d1e15c400625e67b39ba139c8d
milton839 <miltonbiswas839@gmail.com>
2026-07-13 21:46:11 +0600

    String reverse

--- string-reverse.c
+++ string-reverse.c
@@ -13,8 +13,14 @@ void string_reverse(char str[]){
 }
 int main()
 {
-    char str[30] = "Milton";
+    char str[30] = "Milton Biswas";
     // Basic string reverse logic
     string_reverse(str);
+
+    //Library function to reverse string
+    char str1[30] = "Hello World";
+    printf("Original string: %s\n", str1);
+    // strrev(str1); // kaj korbe na because strrev() is not a standard C function and may not be available in all compilers.
+    // printf("Reversed string: %s\n", str1);
     return 0;
 }
