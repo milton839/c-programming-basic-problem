40bcffd94aaa4cb26bbf5278fc10d02f94c5d46e
milton839 <miltonbiswas839@gmail.com>
2026-08-18 10:41:37 +0600

    some optimization

--- rock-paper-scissors-game.c
+++ rock-paper-scissors-game.c
@@ -4,15 +4,22 @@
 int main()
 {
     srand(time(NULL));
-    int yourTurn, computerTurn = rand() % 3 + 1; // Generate a random number between 1 and 3
+    int yourTurn, computerTurn;
     printf("Game Rules: 1 for Rock, 2 for Paper, 3 for Scissors\n\n");
 
-    int yourWinCount = 0, computerWinCount = 0, tieCount = 0;
+    int yourWinCount = 0, computerWinCount = 0, tieCount = 0, invalidInputCount = 0;
 
     for (int i = 0; i < 6; i++)
     {
+        computerTurn = rand() % 3 + 1; // Generate a random number between 1 and 3
         printf("Enter your choice (1-3): ");
         scanf("%d", &yourTurn);
+        if (yourTurn < 1 || yourTurn > 3)
+        {
+            printf("Invalid Input!\n");
+            invalidInputCount++;
+            continue;
+        }
 
         printf("Your choice: %d & Computer's choice: %d\n", yourTurn, computerTurn);
         if (yourTurn == computerTurn)
@@ -35,7 +42,7 @@ int main()
     printf("Your wins: %d\n", yourWinCount);
     printf("Computer wins: %d\n", computerWinCount);
     printf("Ties: %d\n", tieCount);
-
+    printf("Invalid inputs: %d\n", invalidInputCount);
     if (yourWinCount > computerWinCount)
     {
         printf("You win!\n");
