42b3540d1511999c1c64a8b9bf9d7af6955b3ee3
milton839 <miltonbiswas839@gmail.com>
2026-08-19 12:43:40 +0600

    Recursive way string reverse

--- my_str_reverse.c
+++ my_str_reverse.c
@@ -23,19 +23,36 @@ int my_str_length(char *str)
 // }
 
 // Method 2: Static variable use kore return kora
-char *str_Reverse(char *str, int length)
+// char *str_Reverse(char *str, int length)
+// {
+//     static char newString[50];
+//     int i = 0;
+//     while (str[i] != '\0')
+//     {
+//         newString[i] = str[length - 1 - i];
+//         i++;
+//     }
+//     newString[i] = '\0';
+//     printf("%s\n", newString);
+//     return newString;
+// }
+
+void str_Reverse(char *str, int left, int right)
 {
-    static char newString[50];
-    int i = 0;
-    while (str[i] != '\0')
+    if (left >= right)
     {
-        newString[i] = str[length - 1 - i];
-        i++;
+        return;
     }
-    newString[i] = '\0';
-    printf("%s\n", newString);
-    return newString;
+
+    // Swap characters at left and right indices
+    char temp = str[left];
+    str[left] = str[right];
+    str[right] = temp;
+
+    str_Reverse(str, left + 1, right - 1);
 }
+
+
 int main()
 {
     //Method 1: Using a function to reverse a string in simple way
@@ -50,12 +67,21 @@ int main()
 
 
     //Method 2: Static variable use kore return kora
+    // char str[50] = "Hello, World!";
+
+    // int strLength = my_str_length(str);
+    // char *strReverse = str_Reverse(str, strLength);
+
+    // printf("Original String: %s\n", str);
+    // printf("Reversed String: %s\n", strReverse);
+
+    //Method 3: Recursive way to reverse a string
     char str[50] = "Hello, World!";
 
-    int strLength = my_str_length(str);
-    char *strReverse = str_Reverse(str, strLength);
-
     printf("Original String: %s\n", str);
-    printf("Reversed String: %s\n", strReverse);
+    int strLength = my_str_length(str);
+    str_Reverse(str, 0, strLength - 1);
+
+    printf("Reversed String: %s\n", str);
     return 0;
 }
